69d4636f2ff5a9e56fb15137e7242fc1a368e51f
Agus Asso <agusasso22@gmail.com>
2024-09-12 20:07:26 +0900

    belajar html and css

---
+++ script.js
@@ -0,0 +1,19 @@
+
+function perkalian(x, y) {
+    return x * y;
+}
+perkalian(4, 4);
+perkalian(6, 3);
+perkalian(9, 2);
+
+
+
+const btn = document.getElementById
+("btn");
+const tampilan = document.getElementById("tampilan");
+
+btn.onclick = function () {
+    tampilan.innerHTML = "welcome Agus";
+
+   
+}
